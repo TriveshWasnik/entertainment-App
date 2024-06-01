b1e4eff43f9bc7c3ac336ced6644415db390dcdd
TriveshWasnik <triveshwasnik88@gmail.com>
2024-06-02 01:11:28 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1": {
+      "/api": {
         target: "https://entertainment-api-a0nw.onrender.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/v1/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
