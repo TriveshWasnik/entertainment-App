209a932eecb74f4c98e14dc63257bd792e42adb0
TriveshWasnik <triveshwasnik88@gmail.com>
2024-05-31 23:56:46 -0700

    update

--- server/server.js
+++ server/server.js
@@ -17,23 +17,17 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [
-      "https://entertainment-app-890q.onrender.com",
-      "http://localhost:5173",
-      "http://localhost:5174",
-    ],
+    origin: ["", "http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const staticFilesPath = path.join(__dirname, "../dist");
 
-//app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(express.static(staticFilesPath));
 app.get("*", function (req, res) {
-  //res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-  res.sendFile(path.join(staticFilesPath, "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 app.use("/api/v1/user", userRoute);
