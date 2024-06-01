ebf160cc8cbf96266d81149a0e585c2784fd5123
TriveshWasnik <triveshwasnik88@gmail.com>
2024-06-01 20:32:44 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: "https://entertainment-app-api-44s2.onrender.com", //"http://localhost:5000", render backend link
+    proxy: {
+      "/api": {
+        target: "https://entertainment-app-api-44s2.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   plugins: [react()],
 });
