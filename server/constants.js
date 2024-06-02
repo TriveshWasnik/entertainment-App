d4ce252243cd916e5618f3a01a3ed90457bb6ef5
TriveshWasnik <triveshwasnik88@gmail.com>
2024-06-02 15:15:46 +0530

    o

--- server/constants.js
+++ server/constants.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: ".env" });
 
 export const TMDB_BASE_PATH = "https://api.themoviedb.org/3";
 export const API_Option = {
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${process.env.TMDB_TOKEN} `,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjQ2ZjA5ZTgzZDhmMjZmYjQwMDcyMzIiLCJpYXQiOjE3MTczMjExODIsImV4cCI6MTcxNzQwNzU4Mn0.ix0e9jKtV7GUWHiYiwaD_6nrSEcPG6ajd8muQN4eX_o 
+    `,
   },
 };
+
+////${process.env.TMDB_TOKEN}
